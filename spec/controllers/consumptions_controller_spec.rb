d170d934b7be4a40a7d783505a062d66b893a59e
Nabil Allam <nabil.allam@myclimate.org>
2015-01-13 12:33:15 +0100

    test: consumption index and new

--- spec/controllers/consumptions_controller_spec.rb
+++ spec/controllers/consumptions_controller_spec.rb
@@ -10,13 +10,48 @@ RSpec.describe ConsumptionsController, :type => :controller do
       expect(response).to render_template('index')
     end
 
-    it 'assigns @consumptions' do
-      5.times do
-        FactoryGirl.create(:consumption, customer: user.customer)
+    describe 'assigning @consumptions' do
+      before do
+        5.times do
+          FactoryGirl.create(:consumption, customer: user.customer)
+        end
       end
-      consumptions = Consumption.where(customer_id: user.customer_id)
-      get :index, customer_id: user.customer_id
-      expect(assigns(:consumptions)).to eq(consumptions)
+
+      it 'assigns all consumptions' do
+        consumptions = Consumption.where(customer_id: user.customer_id)
+        get :index, customer_id: user.customer_id
+        expect(assigns(:consumptions)).to match_array(consumptions)
+      end
+
+      it 'assigned @consumptions doesn\'t include consumptions from other customers' do
+        other_customer = FactoryGirl.create(:customer)
+        5.times do
+          FactoryGirl.create(:consumption, customer: other_customer)
+        end
+        consumptions = Consumption.where(customer_id: other_customer.id)
+        get :index, customer_id: user.customer_id
+        expect(assigns(:consumptions)).not_to include(consumptions)
+      end
     end
+  end
+
+  describe  'GET new' do
+    let!(:use_sector) { FactoryGirl.create(:use_sector, customer: user.customer) }
+    let!(:other_use_sector) { FactoryGirl.create(:use_sector, customer: user.customer) }
+
+    it 'assingn use_sector consumptions' do
+      5.times do
+        FactoryGirl.create(:consumption, customer: user.customer, use_sector: use_sector)
+      end
+      consumptions = Consumption.where(use_sector_id: use_sector.id)
+      other_consumption = FactoryGirl.create(:consumption, customer: user.customer, use_sector: other_use_sector)
+
+      get :new, customer_id: user.customer_id, use_sector_id: use_sector.id
+
+      expect(assigns(:consumption).attributes).to eq(Consumption.new(use_sector_id: use_sector.id).attributes)
+      expect(assigns(:consumptions)).to match_array(consumptions)
+      expect(assigns(:consumptions)).not_to include(consumptions)
+    end
+
   end
 end
