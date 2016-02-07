a2085f70437bda3a29a7d138caec1ddf54811ca1
Nabil Allam <nabil.allam@myclimate.org>
2016-02-07 11:53:25 +0100

    manager belongs to a customer

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,4 +1,5 @@
 class Manager < ActiveRecord::Base
+  belongs_to :customer
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
