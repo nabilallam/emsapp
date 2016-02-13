38cf517aa3b2e720050848c69156349059239d31
Nabil Allam <nabil.allam@myclimate.org>
2016-02-13 18:02:16 +0100

    prepare consumptions to create columns

--- app/services/chart_service.rb
+++ app/services/chart_service.rb
@@ -1,17 +1,15 @@
 class ChartService
 
-  def initialize(time_period: 'm', date_from: (Date.current - 1.year), date_to: Date.current, customer: nil, use_sector: nil)
-    @time_period = time_period
+  def initialize(time_unit: 'm', date_from: (Date.current - 1.year), date_to: Date.current, customer: nil, use_sector: nil)
+    @time_unit   = time_unit
     @date_from   = date_from
     @date_to     = date_to
     @customer    = customer
     @use_sector  = use_sector
-
-    prepare_columns(consumptions)
   end
 
   def chart_columns
-    [ChartColumnsService.new, ChartColumnsService.new]
+    @chart_columns ||= initialize_columns
   end
 
   private
@@ -24,8 +22,16 @@ class ChartService
       )
     end
 
-    def prepare_columns(consumptions)
-      #@chart_columns ||= Consumption.where(use_sector)
+    def prepared_consumptions
+      @prepared_consumptions ||= prepare
+    end
+
+    def prepare
+      consumptions.select(
+        'CAST (EXTRACT (MONTH FROM effective_date) AS INTEGER) AS month,
+         CAST (EXTRACT (YEAR FROM effective_date) AS INTEGER) AS year,
+         sum(amount) as amount')
+        .group('year, month').order('year, month')
     end
 
     def customer
@@ -44,7 +50,15 @@ class ChartService
       @date_to
     end
 
-    def time_period
-      @time_period
+    def time_unit
+      @time_unit
+    end
+
+    def data_points_count
+      if time_unit == 'm'
+        date_to.month - date_from.month + 1
+      elsif time_unit == 'd'
+        date_to - date_from.month + 1
+      end
     end
 end
--- spec/services/chart_service_spec.rb
+++ spec/services/chart_service_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe ChartService do
       location   = FactoryGirl.create(:location, customer: customer)
       use_sector = FactoryGirl.create(:use_sector)
       resource   = FactoryGirl.create(:resource)
-      create_customer_consumptions(customer, location, use_sector, resource)
+      create_customer_consumptions_monthly(customer, location, use_sector, resource)
 
       values = {customer: customer, location: location, use_sector: use_sector, resource: resource}
       consumption = FactoryGirl.create(
@@ -31,9 +31,27 @@ RSpec.describe ChartService do
       expect(chart_service.send(:consumptions).sum(:amount)).to eql(10 * 111.11)
     end
   end
+
+  describe '#prepare' do
+    it 'returns a grouped result of the consumptions for a customer for a use_sector' do
+      customer   = FactoryGirl.create(:customer)
+      location   = FactoryGirl.create(:location, customer: customer)
+      use_sector = FactoryGirl.create(:use_sector)
+      resource   = FactoryGirl.create(:resource)
+      create_customer_consumptions_daily(customer, location, use_sector, resource)
+
+      values = {customer: customer, location: location, use_sector: use_sector, resource: resource}
+
+      chart_service = ChartService.new(customer: customer, use_sector: use_sector)
+
+      expect(chart_service.send(:prepare).map(&:amount)).to eql [222.22, 222.22, 222.22]
+      expect(chart_service.send(:prepare).first.month).to eql (Date.current - 2.month).month
+      expect(chart_service.send(:prepare).last.month).to eql Date.current.month
+    end
+  end
 end
 
-def create_customer_consumptions(customer, location, use_sector, resource)
+def create_customer_consumptions_monthly(customer, location, use_sector, resource)
   values = {customer: customer, location: location, use_sector: use_sector, resource: resource}
   10.times.each_with_index do |i|
     FactoryGirl.create(
@@ -41,5 +59,18 @@ def create_customer_consumptions(customer, location, use_sector, resource)
       {effective_date: DateTime.current - i.month,
        name: 'Verbrauch 1', amount: 111.11}.merge(values)
     )
+  end
+end
+
+def create_customer_consumptions_daily(customer, location, use_sector, resource)
+  values = {customer: customer, location: location, use_sector: use_sector, resource: resource}
+  [Date.current, Date.current - 1.month, Date.current - 2.month].each do |date|
+    2.times.each do
+      FactoryGirl.create(
+        :consumption,
+        {effective_date: date,
+         name: 'Verbrauch 1', amount: 111.11}.merge(values)
+      )
+    end
   end
 end
